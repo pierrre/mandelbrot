c985c39dea12acc7bace1b555a23b6bfa0b44de4
Pierre Durand <pierredurand@gmail.com>
2014-12-15 00:47:46 +0100

    refactor mandelbrot

--- mandelbrot.go
+++ mandelbrot.go
@@ -22,19 +22,22 @@ func NewMandelbroter(maxIter int) Mandelbroter {
 
 func mandelbrot(c complex128, maxIter int) Result {
 	z := complex(0, 0)
-	var iter int
+	var iter int = 0
 	var absSquare float64
-	for iter = 0; iter < maxIter; iter++ {
-		z = z*z + c
-		// cmplx.Abs(z) is 2x slower
-		absSquare = real(z)*real(z) + imag(z)*imag(z)
-		if absSquare > 4 { // math.Sqrt(4) == 2
+	for {
+		if iter >= maxIter {
 			break
 		}
+		absSquare = real(z)*real(z) + imag(z)*imag(z)
+		if absSquare > 4 {
+			break
+		}
+		z = z*z + c
+		iter++
 	}
 	return Result{
 		Bounded: iter == maxIter,
-		Iter:    iter + 1,
+		Iter:    iter,
 		Abs:     math.Sqrt(absSquare),
 	}
 }
